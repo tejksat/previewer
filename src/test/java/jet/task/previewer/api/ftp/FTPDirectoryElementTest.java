481648cdea1149917a7c833ee10c085eaca6cc88
tejksat <tejksat@users.noreply.github.com>
2015-04-21 00:42:47 +0300

    Make tests pass

--- src/test/java/jet/task/previewer/api/ftp/FTPDirectoryElementTest.java
+++ src/test/java/jet/task/previewer/api/ftp/FTPDirectoryElementTest.java
@@ -1,18 +1,22 @@
 package jet.task.previewer.api.ftp;
 
+import jet.task.previewer.api.DirectoryElement;
 import jet.task.previewer.api.ResolvedDirectory;
 import jet.task.previewer.ftp.FTPClientSession;
 import org.apache.commons.net.ftp.FTPFile;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Future;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -20,13 +24,14 @@ import static org.mockito.Mockito.when;
  * Tests {@link FTPDirectoryElement}.
  */
 public class FTPDirectoryElementTest {
+    @Mock
     private FTPClientSession ftpClientSession;
     private FTPFile ftpFile;
     private FTPDirectoryElement ftpDirectoryElement;
 
     @Before
     public void setUp() throws Exception {
-        ftpClientSession = mock(FTPClientSession.class);
+        MockitoAnnotations.initMocks(this);
         ftpFile = new FTPFile();
         ftpDirectoryElement = new FTPDirectoryElement(ftpClientSession, "/", ftpFile);
     }
@@ -62,20 +67,37 @@ public class FTPDirectoryElementTest {
         Assert.assertFalse(ftpDirectoryElement.canBeResolvedToDirectory());
     }
 
-    @Ignore
     @Test
     public void testResolve() throws Exception {
-        ftpFile.setName("test-folder");
-        Future<List<FTPFile>> futureMock = mock(Future.class);
-        Mockito.when(futureMock.get()).thenReturn(Arrays.asList(new FTPFile()));
-        when(ftpClientSession.changeWorkingDirectory("/test-folder")).thenReturn(futureMock);
-        Future<ResolvedDirectory<?>> resolve = ftpDirectoryElement.resolve(future -> Assert.assertSame(futureMock, future));
+        ftpFile.setName("folder");
+        Future<List<FTPFile>> futureMock = Mockito.mock(Future.class);
+        FTPFile childOne = new FTPFile();
+        childOne.setName("one");
+        FTPFile childTwo = new FTPFile();
+        childTwo.setName("two");
+        List<FTPFile> remoteContent = Arrays.asList(childOne, childTwo);
+        when(futureMock.get()).thenReturn(remoteContent);
+        when(ftpClientSession.changeWorkingDirectory("/folder")).thenReturn(futureMock);
+
+        Future<ResolvedDirectory<?>> resolve = ftpDirectoryElement.resolve(future -> {});
         ResolvedDirectory<?> resolvedDirectory = resolve.get();
+        List<? extends DirectoryElement> resolvedContent = resolvedDirectory.getContent();
+        Assert.assertArrayEquals(
+                remoteContent.stream().map(FTPFile::getName).toArray(),
+                resolvedContent.stream().map(DirectoryElement::getName).toArray()
+        );
     }
 
     @Test
     public void testConsumeInputStream() throws Exception {
-
+        Future consumerMock = mock(Future.class);
+        when(consumerMock.get()).thenReturn("application.log content");
+        ftpFile.setName("application.log");
+        when(ftpClientSession.consumeInputStream(eq("application.log"), any())).thenReturn(consumerMock);
+        String consumerResult = ftpDirectoryElement.<String>consumeInputStream(inputStream -> null);
+        Mockito.verify(ftpClientSession).consumeInputStream(eq("application.log"), any());
+        Mockito.verifyNoMoreInteractions(ftpClientSession);
+        Assert.assertEquals("application.log content", consumerResult);
     }
 
     @Test
